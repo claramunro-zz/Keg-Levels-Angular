eb72ac1ac17f15cc4b7d2e7183fc84df2466524a
Clara Munro and Yulia Shidlovskaya <clara@>
2019-04-03 08:38:34 -0700

    add Keg class

---
+++ src/app/models/keg.model.ts
@@ -0,0 +1,5 @@
+export class Keg{
+  pintLeft: number = 124;
+
+  constructor(public name: string, public brand: string, public price: string , public style: string, public alcoholContent: number){}
+}
